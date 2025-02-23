a49d89ad403ee78bce7bbe8d7d9fb86df6cd2d18
loukastjean <2357611@cegepat.qc.ca>
2025-02-23 18:18:10 -0500

    Commence html et css de base

---
+++ src/figmalogin.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Connexion</title>
+    <link rel="stylesheet" href="./css/style.css" />
+  </head>
+  <body>
+    <div class="main-container">
+      <div class="flex-row-cac">
+        <span class="abc-movies">ABCMovies
+        </span>
+        <div class="search-bar">
+          <input class="search-bar-input" />
+          <div class="search-bar-input-1">
+          </div>
+          <div class="search-icon">
+          </div>
+        </div>
+        <div class="regroup">
+          <button class="create-account-button">
+            <span class="create-account-span">Créer un compte
+            </span>
+          </button>
+          <button class="login-button">
+            <span class="login-span">Connexion
+            </span>
+          </button>
+        </div>
+      </div>
+      <div class="flex-row-a">
+        <div class="register-background">
+          <div class="flex-column">
+            <span class="create-account-heading">Création d’un compte
+            </span>
+            <span class="username-input">Nom d’utilisateur
+            </span>
+            <div class="username-email-bar">
+              <div class="username-email-input">
+              </div>
+            </div>
+            <span class="email-input">Courriel
+            </span>
+            <div class="email-bar">
+              <div class="email-input-2">
+              </div>
+            </div>
+            <span class="password-input">Mot de passe
+            </span>
+            <div class="password-bar">
+              <input class="password-bar-input" />
+              <div class="password-input-3">
+              </div>
+              <div class="eye-password">
+              </div>
+            </div>
+            <button class="enregistrer">
+              <span class="enregistrer-4">Enregistrer
+              </span>
+            </button>
+          </div>
+          <div class="flex-column-a">
+            <div class="info-icon-username">
+            </div>
+            <div class="info-icon-password">
+            </div>
+          </div>
+        </div>
+        <button class="info-username-group">
+          <span class="minimum-maximum-caracteres">Minimum 3 caractères 
+            <br />Maximum 20 caractères 
+          </span>
+        </button>
+        <div class="info-password-group">
+          <span class="minimum-caracteres-letter-number-special">Minimum 8 caractères 
+            <br />Une lettre minuscule 
+            <br />Une lettre majuscule 
+            <br />Un chiffre 
+            <br />Un caractère spécial 
+          </span>
+        </div>
+      </div>
+      <div class="main-background">
+      </div>
+      <div class="nav-bar-background">
+      </div>
+    </div>
+  </body>
+</html>
