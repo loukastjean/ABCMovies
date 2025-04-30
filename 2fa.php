2e61af7d5160962d8abc0e02f58f7dce910d4e27
loukastjean <2357611@cegepat.qc.ca>
2025-04-30 17:18:19 -0400

    Ajout d'une belle interface pour la 2FA

--- 2fa.php
+++ 2fa.php
@@ -4,18 +4,33 @@ if (!ResumeSession("auth")) {
     header("Location: login.php");
     die();
 }
+
+$header = file_get_contents($_SERVER['DOCUMENT_ROOT']."/ABCMovies/common/nav.html");
+
 ?>
+
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="2fa.redirect.php" method="post">
-        <input type="text" name="code">
-        <input type="submit" value="Verify code">
-    </form>
-</body>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>ABCMovies</title>
+    <link rel="stylesheet" href="./css/style.css"/>
+    <script type="text/javascript" src="./js/script.js"></script>
+
+  </head>
+  <body>
+    <?php echo $header ?>
+    <main class="center-div">
+        <form action="2fa.redirect.php" method="post" class="register-login-container">
+            <span class="register-login-title">Validation d'identité</span>
+            <span class="register-login-input-title">Code d'authentification :</span>
+            <div class="register-login-input-container">
+              <input type="text" class="register-login-input" name="code"/>
+            </div>
+            <button type="submit" class="register-login-button">Valider</button>
+        </form>
+    </main>
+  </body>
 </html>
