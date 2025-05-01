aec4638de96a499ce0488650eb811e8b8ef2b194
loukastjean <2357611@cegepat.qc.ca>
2025-05-01 14:24:39 -0400

    Delete classes/config.db.include.php

--- classes/config.db.include.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// OeSwJTxteY][
-
-define("BDUTILISATEUR", "stjeanh25techinf_ABCMovies");
-define("BDPASSWORD", "OeSwJTxteY][");
-define("BDSCHEMA", "stjeanh25techinf_ABCMovies");
-define("BDSERVEUR", "localhost");
